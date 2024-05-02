27bfe35b8a8bfdb45aba599999ad5fed1292ef5a
Etec Prof. Horácio Augusto da Silveira <gorduranogueirapissuto@gmail.com>
2024-05-02 12:45:27 -0300

    Teste

--- coinflipper/coinflipper/Models/Coin.cs
+++ coinflipper/coinflipper/Models/Coin.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Android.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace coinflipper.Models
 {
-    internal class Coin
-    {
+
+    public class Coin() {
+        public string Lado { get; set; }
+        public int LadoEscolhido { get; set; }
     }
+    public Coin()
+    {
+
+    }
+    public Jogar()
+    {
+        Lado = (new Random().Next(2) == 0) ? "Cara" : "Coroa";
+        return Lado;
+    }
+
 }
